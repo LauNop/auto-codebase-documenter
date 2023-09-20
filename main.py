a0dcdfe30adf7e5158785ecdb7f165a4c3d03802
Laurent Nopoly <Laurent.Nopoly@gbh.fr>
2023-09-20 18:59:29 -0400

    create get_files function

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import os
 import fnmatch
 
+
 def forward_slash_to_backslash(input_string):
     # Replace all forward slashes with backslashes
     result = input_string.replace('/', '\/')
     return result
 
-def get_files(root_dir, ignore_patterns=None):
+
+def get_files(root_dir, ignore_list=[], unwanted_file=[]):
     # Initialize a list to store the file paths
+    if unwanted_file is None:
+        unwanted_file = []
     file_list = []
 
     # Create a set to store the patterns from .gitignore
-    gitignore_patterns = set()
+    ignore_patterns = set()
 
     # Check if a .gitignore file exists and add its patterns to the set
     gitignore_path = os.path.join(root_dir, ".gitignore")
@@ -20,42 +24,37 @@ def get_files(root_dir, ignore_patterns=None):
             for line in gitignore_file:
                 line = line.strip()
                 if line and not line.startswith("#"):  # Ignore comments and empty lines
-                    line = forward_slash_to_backslash(line)
-                    gitignore_patterns.add(line)
+                    ignore_patterns.add(line)
 
     # If additional ignore patterns are provided, add them to the set
-    if ignore_patterns:
-        gitignore_patterns.update(ignore_patterns)
+    if ignore_list:
+        ignore_patterns.update(ignore_list)
 
-    print(gitignore_patterns)
+    print(ignore_patterns)
     # Walk through the root directory and its subdirectories
     count = 0
-    for root, dirs, files in os.walk(root_dir):
-        for filename in files:
-            file_path = os.path.join(root, filename)
-            relative_path = os.path.relpath(file_path, root_dir)
-            print("File path:",file_path)
-            print("Relative path:",relative_path)
-
-
-            # Check if the file matches any of the ignore patterns
-            if any(fnmatch.fnmatchcase(relative_path, pattern) for pattern in gitignore_patterns):
-                continue  # Skip files that match the ignore patterns
-            print(count)
-            count += 1
-            file_list.append(file_path)
-
+    for dirpath, dirnames, filenames in os.walk(root_dir):
+        for ignore_pattern in ignore_patterns:
+            if ignore_pattern in dirnames:
+                dirnames.remove(ignore_pattern)
+        file_list.extend(
+            [
+                os.path.join(dirpath, filename)
+                for filename in filenames
+                if any(filename.endswith(file_type) for file_type in [".py"]) and filename not in unwanted_file
+            ]
+        )
     return file_list
 
 
 # Usage example:
 if __name__ == "__main__":
-
     root_directory = "."
-    additional_ignore_patterns = ["*.txt", "*.md","setup.py"]  # Add any additional patterns to ignore
-    files = get_files(root_directory, additional_ignore_patterns)
+    additional_ignore_patterns = ["tests", ".git"]  # Add any additional patterns to ignore
+    unwanted_file = ["setup.py", "__init__.py"]
+    files = get_files(root_directory, additional_ignore_patterns,unwanted_file)
 
     # Print the list of files
-    # for file in files:
-    #     print(file)
+    for file in files:
+        print(file)
     print(len(files))
