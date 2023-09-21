ce5d9c18e06f7eb9f0fcfdfc486afede9653becf
La_Nopoly <laurent.nopoly@gbh.fr>
2023-09-21 10:54:04 -0400

    update docu prompt template

--- my_auto_codebase_documenter/default_ai_prompt.py
+++ my_auto_codebase_documenter/default_ai_prompt.py
@@ -12,62 +12,62 @@ default_ai_prompt = [
         Loads the application configuration etc...
         
         ## Codebase
-        if there is imports in the file you write this section below
-        ### Imports: 
-                
-            1. ```python from some_module import some_function
-                ```
-                Explain what the some_module is usually used for and what is his purpose in this file...
-                
-                
-            2. ```python import some_module
-               ```
-                Explain what the some_module is usually used for and what is his purpose in this file...
-       ***
-       
-        if there is classes in the file you write this section below
-        ### Classes
-            #### Class: frameObj 
-    
-            Purpose: Some description here...
-                #### Methods: method of the class
-                    1. ```python __init__ (list vars here...) ```
+            if there is imports in the file you write this section below
+            ### Imports: 
                     
-                      Initialises the class and etc...
-                      ##### Variables:
-                      Explain the purpose of the variables in the function as follow :
-                        - self.some_variable : its purpose ...
-                        - another_variable : its purpose ...
-                      
-                    2. ```python addObject (list vars here...) ```
-                      write as the example above
-                      
+                1. ```python from some_module import some_function
+                    ```
+                    Explain what the some_module is usually used for and what is his purpose in this file...
+                    
+                    
+                2. ```python import some_module
+                   ```
+                    Explain what the some_module is usually used for and what is his purpose in this file...
+       *    **
+        
+            if there is classes in the file you write this section below
+            ### Classes
+                #### Class: frameObj 
+        
+                Purpose: Some description here...
+                    #### Methods: method of the class
+                        1. ```python __init__ (list vars here...) ```
+                        
+                          Initialises the class and etc...
+                          ##### Variables:
+                          Explain the purpose of the variables in the function as follow :
+                            - self.some_variable : its purpose ...
+                            - another_variable : its purpose ...
+                          
+                        2. ```python addObject (list vars here...) ```
+                          write as the example above
+                          
+                
+                #### Class: anotherframeObj 
+        
+                write as the example above
+            ***
             
-            #### Class: anotherframeObj 
-    
-            write as the example above
-        ***
+            if there's methods outside of a class in the file you write this section below 
+            ### Methods:  
+                #### Method: ```python some_method (list vars here...) ```
         
-        if there's methods outside of a class in the file you write this section below 
-        ### Methods:  
-        #### Method: some_method (list vars here...)
-
-        Purpose: Some description about what this method does...
+                Purpose: Some description about what this method does...
+                
+                #### Method: ```python another_method (list vars here...) ```
         
-        #### Method: another_method (list vars here...)
-
-        Purpose: Some description about what this method does...
+                Purpose: Some description about what this method does...
+            
+            ***
+            if there's global variables outside of function and class in the file you write this section below
+            ### Variables:
+                #### Variable: SOME_VARIABLE
         
-        ***
-        if there's global variables outside of function and class in the file you write this section below
-        ### Variables:
-        #### Variable: SOME_VARIABLE
-
-        Purpose: Some description about what this variable does...
+                Purpose: Some description about what this variable does...
+                
+                #### Variable: ANOTHER_VARIABLE
         
-        #### Variable: ANOTHER_VARIABLE
-
-        Purpose: Some description about what this variable does...
+                Purpose: Some description about what this variable does...
         
         """,
 ]
